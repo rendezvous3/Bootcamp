be517bf5ab2ea257b26dbce354454ca3c2ea72b4
Bojan <bojanj6m@gmail.com>
2017-03-06 23:21:13 -0500

    Created Core, Feeds App, model Feeds

---
+++ App/bootcamp/feeds/models.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Feed(models.Model):
+	user = models.ForeignKey(User)
+	date = models.DateTimeField(auto_now_add=True)
+	post = models.TextField(max_length=2000)
+	parent = models.ForeignKey('Feed', null=True, blank=True)
+
+	class Meta:
+		verbose_name="Feed"
+		verbose_name_plural="Feeds"
+		ordering = ("-date", )
+
+	def __unicode__(self):
+		return self.post
+
+	def get_comments(self):
+		return Feed.objects.filter(parent=self.pk)		
