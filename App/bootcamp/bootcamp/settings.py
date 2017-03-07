0f51e93f7d1d604d568e70247e7a2b5589b9c609
Bojan <bojanj6m@gmail.com>
2017-03-07 13:39:29 -0500

    modified settings.py, home.html, base.html

--- App/bootcamp/bootcamp/settings.py
+++ App/bootcamp/bootcamp/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(__file__)
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'core',
     'feeds'
 ]
