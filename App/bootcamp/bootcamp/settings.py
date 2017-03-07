9484442e567646f6b914ae95f2ebcc8a52deba00
Bojan <bojanj6m@gmail.com>
2017-03-07 14:30:43 -0500

    Created Questions, Ansers model, questions app

--- App/bootcamp/bootcamp/settings.py
+++ App/bootcamp/bootcamp/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'feeds'
+    'feeds',
+    'questions'
 ]
 
 MIDDLEWARE = [
