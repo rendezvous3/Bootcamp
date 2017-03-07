4dbd54134458308fe5cc551c2fd8a399920ff3fa
Bojan <bojanj6m@gmail.com>
2017-03-07 14:09:47 -0500

    Deleted core app, movedviews and templates to feeds app

--- App/bootcamp/bootcamp/urls.py
+++ App/bootcamp/bootcamp/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from core.views import home
+from feeds.views import home
 
 urlpatterns = [
 	url(r'^$', home, name="home"),
