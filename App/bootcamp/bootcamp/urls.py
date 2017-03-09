a18e6955110710fbc874c2f6c9290ef23aab7fe9
Bojan <bojanj6m@gmail.com>
2017-03-09 02:16:18 -0500

    Setup static files

--- App/bootcamp/bootcamp/urls.py
+++ App/bootcamp/bootcamp/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static    
 
 from feeds.views import home, post
 from questions.views import questions, ask, question, answered, unanswered
@@ -30,3 +32,6 @@ urlpatterns = [
     url(r'^questions/ask/$', ask, name="ask"),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
